5a11d33add1db7165c52c06785c9ec7d2dd70cf0
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-09-26 19:26:21 -0300

    Improving dashboard page

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,19 +10,19 @@ import {
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
-      <Stack spacing="12" align="flex-start">
+      <Stack spacing="12">
         <Box>
           <Text fontWeight="bold" color="gray.400" fontSize="small">
             GERAL
           </Text>
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
+          <Stack spacing="4" mt="8" alignItems="stretch">
+            <Link display="flex" alignItems="center">
               <Icon as={RiDashboardLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Dashboard
               </Text>
             </Link>
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiContactsLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Usuários
@@ -34,14 +34,14 @@ export function Sidebar() {
           <Text fontWeight="bold" color="gray.400" fontSize="small">
             AUTOMAÇÃO
           </Text>
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
+          <Stack spacing="4" mt="8" alignItems="stretch">
+            <Link display="flex" alignItems="center">
               <Icon as={RiInputMethodLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Formulários
               </Text>
             </Link>
-            <Link display="flex" align="center">
+            <Link display="flex" alignItems="center">
               <Icon as={RiGitMergeLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Automação
