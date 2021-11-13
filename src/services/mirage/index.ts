e5c550450eb5e18028807404c0f9f406e1b941c7
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-11-13 19:31:49 -0300

    Creating factory and seed data in mirage with faker

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,10 +1,21 @@
-import { createServer, Model, Server } from "miragejs";
+import { createServer, Factory, Model, Server } from "miragejs";
 import { User } from "../../models/user";
+import { internet, date } from "faker";
 
 export function makeServer() {
   const server = createServer({
     models: {
       user: Model.extend<Partial<User>>({}),
+    },
+    factories: {
+      user: Factory.extend({
+        name: (index: number) => `User ${index + 1}`,
+        email: (index: number) => internet.email().toLowerCase(),
+        created_at: () => date.recent(2),
+      }),
+    },
+    seeds(server: Server) {
+      server.createList("user", 50);
     },
     routes() {
       this.namespace = "api";
