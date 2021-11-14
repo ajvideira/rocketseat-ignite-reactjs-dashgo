dae9325e7259edb5abaff8f14c53384056c5eee2
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-11-14 06:51:04 -0300

    Adding spinner on refetching users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,7 +25,7 @@ import { Header } from "../../components/Header";
 import { User } from "../../models/user";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery<User[]>(
+  const { data, isLoading, isFetching, error } = useQuery<User[]>(
     "users",
     async () => {
       const response = await fetch("/api/users");
@@ -56,7 +56,10 @@ export default function UserList() {
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
-              Usuários
+              Usuários{" "}
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
